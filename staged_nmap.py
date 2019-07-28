fd6b6a5cb825a62da86501b3eb32542ee2987dc8
xaithe <>
2019-07-28 22:26:20 +0100

    1st version of script finished

--- staged_nmap.py
+++ staged_nmap.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+import os
 import argparse
 import subprocess
 import xml.etree.ElementTree as ET
@@ -10,11 +10,14 @@ parser.add_argument("host", help="The IP address or hostname of the scan target"
 args = parser.parse_args()
 
 host = args.host
-
+wd = os.getcwd()
+filename = wd + "/stagednmap.txt"
 # run initial nmap scan and get the output
 
-initialscan = subprocess.run(['nmap', '-T4','-p-','-oX','-',host], stdout=subprocess.PIPE)
-initialresult = initialscan.stdout.decode('utf-8')
+print("Running initial scan on {}".format(host))
+
+initialscan = subprocess.run(["nmap","-T4","-p-","-oX","-",host], stdout=subprocess.PIPE)
+initialresult = initialscan.stdout.decode("utf-8")
 
 # parse the output and grab open ports
 
@@ -24,4 +27,15 @@ openports = list()
 for port in root.findall(".//port"):
 	 openports.append(port.attrib["portid"])
 
+portparam = "-p{}".format(",".join(openports))
 
+# Run deep scan
+print("Running final scan on {}".format(host))
+
+finalscan = subprocess.run(["nmap","-A",portparam,"-oN","-",host], stdout=subprocess.PIPE)
+result = finalscan.stdout.decode("utf-8")
+
+with open(filename, "w") as textout:
+	textout.write(result)
+
+print("Output saved to {}".format(filename))
